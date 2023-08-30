62ca5dc9bd981e7afcf073e384283bc70df36e2b
Robert Taylor <trobert844@gmail.com>
2023-08-30 08:23:53 -0400

    add ability and moves

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ console.log(pokédex);
 const fetchPokémon = () => {
 
     const promises = [];
-    for (let i = 1; i <= 250; i++) {
+    for (let i = 1; i <= 384; i++) {
         const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
         promises.push(fetch(url).then((res) => res.json()));
     }    
@@ -17,7 +17,9 @@ const fetchPokémon = () => {
             image: data.sprites['front_default'],
             type: data.types.map( type => type.type.name).join(', '),
             height: data.height,
-            width: data.weight
+            width: data.weight,
+            ability: data.abilities.map(ability => ability.ability.name).join(','),
+            moves: data.moves.map(move => move.move.name).slice(0, 10).join(', ')
         }));
         displayPokemon(pokémon);
     });
@@ -30,6 +32,8 @@ const displayPokemon = (pokémon) => {
             <img class="card-image" src="${pokéman.image}"/>
             <h2 class="card.title">${pokéman.id}. ${pokéman.name}</h2>
             <p class="card-subtitle">Type: ${pokéman.type}</p>
+            <p class="card-subtitle">Ability: ${pokéman.ability}</p>
+            <p class="card-subtitle">Moves: ${pokéman.moves}</p>
         </li>
     `).join('');
     pokédex.innerHTML = pokémonHTMLString;
